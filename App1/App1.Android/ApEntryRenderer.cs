8902c465df1971ae0cbf51789a6f65176405ea57
apolo96 <andresfpvclap@gmail.com>
2017-09-02 09:35:05 -0500

    Add project files.

---
+++ App1/App1.Android/ApEntryRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using App1.Droid;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(ApEntryRenderer))]
+namespace App1.Droid
+{
+    class ApEntryRenderer: EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = Android.App.Application.Context.GetDrawable(Resource.Drawable.crentry);
+                Control.Gravity = GravityFlags.CenterVertical;
+                Control.SetPadding(10,10,10,10);
+            }
+        }
+    }
+}
---
+++ App1/App1.Android/CustomRenderers/AEntryRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using App1.Droid.CustomRenderers;
+using App1.CustomRenderers;
+
+[assembly: ExportRenderer(typeof(ACustomEntry), typeof(AEntryRenderer))]
+namespace App1.Droid.CustomRenderers
+{
+    class AEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = Android.App.Application.Context.GetDrawable(Resource.Drawable.crentry);
+                Control.Gravity = GravityFlags.CenterVertical;
+                Control.SetPadding(10, 10, 10, 10);
+            }
+        }
+    }
+}
